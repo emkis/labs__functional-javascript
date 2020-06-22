18c3bd068eab3d90b338f23830f63dd4977649c0
Emkis <nicolasemkis@gmail.com>
2020-06-22 01:46:40 -0300

    make es6 sintax

--- most-frequent-words/src/app.js
+++ most-frequent-words/src/app.js
@@ -1,5 +1,6 @@
-const { join } = require('path')
+import { join } from 'path'
+import { getFilesFromDirectory } from './utils/functions'
 
 const subtitlesPath = join(__dirname, '..', 'assets', 'subtitles')
 
-console.log(subtitlesPath)
+const files = getFilesFromDirectory(subtitlesPath).then(console.log)
