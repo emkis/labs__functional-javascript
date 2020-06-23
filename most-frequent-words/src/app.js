a5e990ae5c7aad3bb6fff73370fd000b59cf03eb
Emkis <nicolasemkis@gmail.com>
2020-06-23 00:28:41 -0300

    lint file and remove unecessary consts

--- most-frequent-words/src/app.js
+++ most-frequent-words/src/app.js
@@ -2,43 +2,22 @@ import { join } from 'path'
 import * as utils from './utils/functions'
 
 const subtitlesDirectory = join(__dirname, '..', 'assets', 'subtitles')
+const patternsToRemove = ['.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')']
 const filesExtension = '.srt'
-const patternsToRemove = [
-  '.', '?', '-', ',', '"', '♪',
-  '_', '<i>', '</i>', '\r',
-  '[', ']', '(', ')'
-]
 
-function makeWordsGroup (words) {
-  const initialState = {}
-
-  return Object.values(words.reduce((groupedWords, word) => {
-    const text = word.toLowerCase()
-    const quantity = groupedWords[text] ? groupedWords[text].quantity + 1 : 1
-
-    groupedWords[text] = { word: groupedWords[text], quantity }
-
-    return groupedWords
-  }, initialState))
-}
-
-async function getWordsFromSubtitles (directory, targetExtension) {
-  const allFiles = await utils.getFilesFromDirectory(directory)
-  const targetFiles = await utils.filesEndingWith(allFiles, targetExtension)
-  const subtitles = await utils.readFiles(targetFiles)
-  const mergedContent = utils.mergeContent(subtitles)
-  const splittedContent = utils.splitStringByPattern(mergedContent, '\n')
-  const stringsWithoutOnlySpaces = utils.deleteEmptyStrings(splittedContent.map(string => string))
-  const stringsWithoutDuration = utils.deleteWhenIncludes(stringsWithoutOnlySpaces, '-->')
-  const stringsWithoutNumbers = utils.deleteWhenHasOnlyNumber(stringsWithoutDuration)
-  const sanitizedStrings = utils.deletePatternsFromTexts(stringsWithoutNumbers, patternsToRemove)
-  const sanatizedMergedContent = utils.mergeContent(sanitizedStrings)
-  const splittedWords = utils.splitStringByPattern(sanatizedMergedContent, ' ')
-  const wordsNotEmpty = utils.deleteEmptyStrings(splittedWords)
-  const wordsWithoutNumbers = utils.deleteWhenHasOnlyNumber(wordsNotEmpty)
-  const groupedWords = makeWordsGroup(wordsWithoutNumbers)
-
-  return groupedWords
-}
-
-getWordsFromSubtitles(subtitlesDirectory, filesExtension).then(console.log)
+utils.getFilesFromDirectory(subtitlesDirectory)
+  .then(utils.textsEndingWith(filesExtension))
+  .then(utils.readFiles)
+  .then(utils.mergeContent)
+  .then(utils.splitStringByPattern('\n'))
+  .then(utils.deleteEmptyStrings)
+  .then(utils.deleteWhenIncludes('-->'))
+  .then(utils.deleteWhenHasOnlyNumber)
+  .then(utils.deletePatternsFromTexts(patternsToRemove))
+  .then(utils.mergeContent)
+  .then(utils.splitStringByPattern(' '))
+  .then(utils.deleteEmptyStrings)
+  .then(utils.deleteWhenHasOnlyNumber)
+  .then(utils.groupWords)
+  .then(utils.orderByAttrNumeric('quantity', 'desc'))
+  .then(console.log)
