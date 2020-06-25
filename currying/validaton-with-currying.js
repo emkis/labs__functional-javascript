f01f45584be5a984810ecafdf768c6fffff21151
Emkis <nicolasemkis@gmail.com>
2020-06-25 03:32:01 -0300

    add currying functions

---
+++ currying/validaton-with-currying.js
@@ -0,0 +1,43 @@
+/**
+ * regular function to validate some text
+ * BUT, using currying to make more reusable
+ */
+
+function validateTextBetween(minSize) {
+  return function (maxSize) {
+    return function (errorMessage) {
+      return function (text) {
+        const textSize = (text || '').trim().length
+
+        if (textSize < minSize || textSize > maxSize) {
+          throw errorMessage
+        }
+
+        return true
+      }
+    }
+  }
+}
+
+function applyValidation(callback) {
+  return function (value) {
+    try {
+      callback(value)
+    } catch (error) {
+      return error
+    }
+  }
+}
+
+const MIN_SIZE = 4
+const MAX_SIZE = 12
+const ERROR_MESSAGE = 'Input is invalid'
+
+const forceDefaultProductNameSize = validateTextBetween(MIN_SIZE)(MAX_SIZE)
+const forceValidProductName = forceDefaultProductNameSize(ERROR_MESSAGE)
+const validateProductName = applyValidation(forceValidProductName)
+
+const text = 'Lorem ipsum sit dollor'
+const isValid = validateProductName(text)
+
+console.log(isValid)
