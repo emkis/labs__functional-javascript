30959e0db59d9b921b7c9774e764cc2fbc106243
Emkis <nicolasemkis@gmail.com>
2020-06-25 04:02:28 -0300

    add lazy evaluation functions

---
+++ lazy-evaluation/index.js
@@ -0,0 +1,47 @@
+// sample usage of lazy evaluation concept
+
+function eagerRobustCalculation (valueA, valueB) {
+  const timeToEnd = Date.now() + 2_500
+  const poweredValue = Math.pow(valueA, 3)
+
+  // just for slow things out for better demonstration
+  while (Date.now() < timeToEnd) continue
+
+  return poweredValue + valueB
+}
+
+function lazyRobustCalculation (valueA) {
+  const timeToEnd = Date.now() + 2_500
+  const poweredValue = Math.pow(valueA, 3)
+
+  // just for slow things out for better demonstration
+  while (Date.now() < timeToEnd) continue
+
+  return function (valueB) {
+    return poweredValue + valueB
+  }
+}
+
+console.time('#eager function')
+
+console.log( eagerRobustCalculation(3, 100) ) // 127
+console.log( eagerRobustCalculation(3, 200) ) // 227
+console.log( eagerRobustCalculation(3, 300) ) // 227
+
+console.timeEnd('#eager function')            // 7507.036ms
+
+/**
+ * in the lazy function, the idea is that we calculate
+ * all the values only when necessary, and we can reuse
+ * the first function result for another cases
+ */
+
+console.time('#lazy evaluation')
+
+const lazyPoweredThree = lazyRobustCalculation(3)
+
+console.log( lazyPoweredThree(100) ) // 127
+console.log( lazyPoweredThree(200) ) // 227
+console.log( lazyPoweredThree(300) ) // 327
+
+console.timeEnd('#lazy evaluation')  // 2499.923ms
